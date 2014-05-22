d3b463873beabb8cec54979a752b089a80fc3e75
deniskulicek <denis.kulicek@gmail.com>
2014-05-22 23:43:53 +0200

    fixed ports

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
-var port = process.env.POST || 3333;
 
+app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/dist'));
 
-app.listen(port, function(){
-	console.log('Express server listening on port: ' + port);
+app.listen(app.get('port'), function(){
+	console.log('Express server listening on port ' + app.get('port'));
 });
