7c2165cef1ffd27e972d471a4806948211bce926
deniskulicek <denis.kulicek@gmail.com>
2014-05-23 00:08:26 +0200

    https forced app.js

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/dist'));
 
+/* Force redirect to https */
+app.get('*', function(req, res, next){
+	if((process.env.NODE_ENV === 'production') && (req.headers['x-forwarded-proto']!=='https')){
+		res.redirect('https://ftn.herokuapp.com' + req.url);
+	} else {
+		next();
+	}
+});
+
 app.listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
+	console.log('Process env: ' + process.env.NODE_ENV);
 });
