1884c25428d4ede7b2c735d740a73238ee8b36ce
deniskulicek <denis.kulicek@gmail.com>
2014-05-23 00:34:02 +0200

    fixed https thing

--- app.js
+++ app.js
@@ -6,9 +6,8 @@ app.set('port', process.env.PORT || 3000);
 /* Force redirect to https */
 app.get('*', function(req, res, next){
 
-	console.log('Request to: '+req.url, 'x-forwarded-proto: ' + req.headers);
-
 	if((process.env.NODE_ENV === 'production') && req.headers['x-forwarded-proto']!=='https'){
+		console.log('redirecting: ' + req.url + ' to HTTPS.');
 		res.redirect('https://ftn.herokuapp.com' + req.url);
 	} else {
 		next();
