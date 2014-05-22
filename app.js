940145675c41f65af05cf1db8eafdb71a8a0a5cf
deniskulicek <denis.kulicek@gmail.com>
2014-05-23 00:26:31 +0200

    fixed https thing

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ var express = require('express');
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
-app.use(express.static(__dirname + '/dist'));
 
 /* Force redirect to https */
 app.get('*', function(req, res, next){
 
-	console.log('Request to: '+req.url+' is secore: ' + req.secure);
+	console.log('Request to: '+req.url+' is secure: ' + req.secure);
 
 	if((process.env.NODE_ENV === 'production') && !req.secure){
 		res.redirect('https://ftn.herokuapp.com' + req.url);
@@ -15,6 +14,8 @@ app.get('*', function(req, res, next){
 		next();
 	}
 });
+
+app.use(express.static(__dirname + '/dist'));
 
 app.listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
