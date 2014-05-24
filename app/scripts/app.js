bcc1eec89c7136ab1f1b4787aa7a059eced95229
deniskulicek <denis.kulicek@gmail.com>
2014-05-24 15:38:01 +0200

    fixed route to rest api

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,7 +8,7 @@
 
 *********/
 
-var serverAddress = 'http://localhost:3000'; //'https://ftn-13190.onmodulus.net';
+var serverAddress = 'https://ftn-13190.onmodulus.net'; //'http://localhost:3000'; dev
 var postsPath = '/posts'; //based on useSampleData value
 
 var geoLocationServiceAddress = 'https://maps.googleapis.com/maps/api/geocode/json?';
